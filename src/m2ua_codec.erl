9c9553ca14588eddd12b4144fbeb5411eb03e609
Harald Welte <laforge@gnumonks.org>
2011-01-18 18:24:09 +0100

    M2UA: various fixes in the parser and encoder, mostly related to padding

--- src/m2ua_codec.erl
+++ src/m2ua_codec.erl
@@ -23,15 +23,36 @@
 
 -export([parse_m2ua_msg/1, encode_m2ua_msg/1]).
 
+% compute the number of pad bits required after a binary parameter
+get_num_pad_bytes(BinLenBytes) ->
+	case BinLenBytes rem 4 of
+		0 ->	0;
+		Val -> 	4 - Val
+	end.
+
 % parse a binary chunk of options into an options proplist
-parse_m2ua_opts(<<>>, OptList) ->
+parse_m2ua_opts(<<>>, OptList) when is_list(OptList) ->
 	OptList;
-parse_m2ua_opts(OptBin, OptList) ->
+parse_m2ua_opts(OptBin, OptList) when is_list(OptList) ->
 	<<Tag:16/big, LengthIncHdr:16/big, Remain/binary>> = OptBin,
 	Length = LengthIncHdr - 4,
-	<<Value:Length/binary, NextOpts/binary>> = Remain,
+	PadLength = get_num_pad_bytes(Length),
+	%io:format("Tag ~w, LenInHdr ~w, Len ~w, PadLen ~w, Remain ~w(~p)~n",
+	%	  [Tag, LengthIncHdr, Length, PadLength, byte_size(Remain), Remain]),
+	<<Value:Length/binary, PadNextOpts/binary>> = Remain,
+	% this is ridiculous, we cannot use "<<Value:Length/binary,
+	% 0:PadLength, Remain/binary>>" as the last part would not match an
+	% empty binary <<>> anymore.  Without the "0:PadLengh" this works
+	% perfectly fine.  Now we need some complicated construct and check if
+	% the resulting list would be empty :((
+	if
+		byte_size(PadNextOpts) > PadLength ->
+			<<0:PadLength/integer-unit:8, NextOpts/binary>> = PadNextOpts;
+		true ->
+			NextOpts = <<>>
+	end,
 	NewOpt = {Tag, {Length, Value}},
-	parse_m2ua_opts(NextOpts, [NewOpt|OptList]).
+	parse_m2ua_opts(NextOpts, OptList ++ [NewOpt]).
 
 % parse a single M2UA message
 parse_m2ua_msgt(_, _, _, Remain) ->
@@ -48,20 +69,24 @@ parse_m2ua_msg(DataBin) when is_binary(DataBin) ->
 % encode a single option
 encode_m2ua_opt({OptNum, {DataBinLen, DataBin}}) when is_integer(OptNum) ->
 	LengthIncHdr = DataBinLen + 4,
-	<<OptNum:16/big, LengthIncHdr:16/big, DataBin/binary>>.
+	PadLength = get_num_pad_bytes(DataBinLen),
+	case PadLength of
+		0 -> <<OptNum:16/big, LengthIncHdr:16/big, DataBin/binary>>;
+		_ -> <<OptNum:16/big, LengthIncHdr:16/big, DataBin/binary, 0:PadLength/integer-unit:8>>
+	end.
 
 % encode a list of options
 encode_m2ua_opts([], OptEnc) ->
-	list_to_binary(OptEnc);
+	OptEnc;
 encode_m2ua_opts([CurOpt|OptPropList], OptEnc) ->
 	CurOptEnc = encode_m2ua_opt(CurOpt),
-	encode_m2ua_opts(OptPropList, list_to_binary([OptEnc, CurOptEnc])).
+	encode_m2ua_opts(OptPropList, <<OptEnc/binary, CurOptEnc/binary>>).
 	
 
 % encode a particular message type
 encode_m2ua_msgt(MsgClass, MsgType, Params) ->
 	OptBin = encode_m2ua_opts(Params, <<>>),
-	MsgLenIncHdr = 4 + binary:byte_size(OptBin),
+	MsgLenIncHdr = 8 + byte_size(OptBin),
 	<<1:8, 0:8, MsgClass:8, MsgType:8, MsgLenIncHdr:32/big, OptBin/binary>>.
 
 % encode a message from record to binary
